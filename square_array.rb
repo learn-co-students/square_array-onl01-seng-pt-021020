f7317140ddf1118908016d03d1af89c41bea9c11
Daniel Sasser <danielbsasser@gmail.com>
2020-02-12 02:01:32 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  
+  squared_numbers = []
+  
+  array.each do |number|
+    squared_numbers << number * number 
+  end
+  
+  return squared_numbers
+  
 end
