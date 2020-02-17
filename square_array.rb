6f889ba01d8405544c6ac0aa2f826dd24a200972
Gilberto Ocampo <ocampo@gate.net>
2020-02-17 01:45:36 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+## def square_array(array)
+  ## new_arr = []
+  ## array.each { |number| new_arr << number ** 2 }
+  ## new_arr
+## end
+
 def square_array(array)
-  # your code here
+  new_arr = []
+  array.each { |number| new_arr << number ** 2 }
+  new_arr
 end
+
