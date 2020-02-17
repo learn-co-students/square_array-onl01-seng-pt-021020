5c8d8191a74a1380e55a7f899ad55e6c9dd86fd0
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-17 04:49:39 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  squared_numbers = []
+  
+  numbers.each do |number|
+    squared_numbers.push(number**2)
+  end
+  return squared_numbers
+end
+
+square_array([1, 2, 3, 4, 5])
+
+def squaring(integers)
+    integers.collect {|integer| integer**2} 
 end
